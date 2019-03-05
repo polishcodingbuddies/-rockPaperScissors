e3e5b2956a700acd9e821c0331b7adf7f443040a
Szymon Gąsior <szgasior@outlook.com>
2019-03-05 18:18:31 +0100

    Working on game in JS

--- index.js
+++ index.js
@@ -32,18 +32,79 @@ function setRandomAvatarForPlayers() {
 
 
 function countingDown() {
+    //hide button 'Fight'
+    document.getElementById("play").style.visibility = "hidden";
 
-    var counter = 10;
+    var counter = 6;
     var run = setInterval(function () {
         document.querySelector(".gameResult").innerHTML = counter;
+
+
+        if (isRockChoiceDone || isPaperChoiceDone || isScissorsChoiceDone) {
+            document.querySelector(".gameResult").innerHTML = roundResult();
+            clearInterval(run);
+        }
+
         if (counter === 0) {
-            document.querySelector(".gameResult").innerHTML = "RESULT";
+
+            //random choice TODO     
+            document.querySelector(".gameResult").innerHTML = roundResult();
             clearInterval(run);
         }
         counter--
     }, 1000);
+
+}
+// hardcoded COMPUTER HAS ALWAYS ROCK
+// hardcoded PLAYER HAS ALWAYS PAPER
+function roundResult() {
+
+    //    //visible button 'Fight'
+    document.getElementById("play").style.visibility = "visible";
+
+
+    //TODO -random computer choice and check who won
+    var computerChoice = "Rock";
+    isPaperChoiceDone = true;
+
+
+
+    //todo end the if statement
+    if (isPaperChoiceDone && (computerChoice == "Rock")) {
+        playerPoints++;
+    } else if (isPaperChoiceDone && (computerChoice == "Scissors")) {
+        computerPoints++;
+    } else if (isPaperChoiceDone && (computerChoice == "Paper")) {
+        computerPoints = computerPoints;
+    } else if (isScissorsChoiceDone && (computerChoice == "Rock")) {
+        computerPoints++;
+    } else if (isScissorsChoiceDone && (computerChoice == "Paper")) {
+        playerPoints++;
+    } else if (isScissorsChoiceDone && (computerChoice == "Scissors")) {
+        computerPoints = computerPoints;
+    }
+
+    if (computerPoints == 3) {
+        return "Computer won";
+    } else if (playerPoints == 3) {
+        return "Player won";
+    } else {
+        return playerPoints + " : " + computerPoints;
+    }
+
+}
+
+function startGame() {
+    setRandomAvatarForPlayers();
 }
 
 
+// game variables
 
-setRandomAvatarForPlayers();
+var computerPoints = 0;
+var playerPoints = 0;
+var isRockChoiceDone = false;
+var isPaperChoiceDone = false;
+var isScissorsChoiceDone = false;
+
+startGame();
